c7d83ee2daa7ec149231e364726b931d1aba6166
isaque <isaquenobrec@gmail.com>
2025-05-22 14:30:57 -0300

    outro commit desafio fundamentos

--- desafio_dio_basico/ContaPoupanca.java
+++ desafio_dio_basico/ContaPoupanca.java
@@ -9,5 +9,17 @@ package desafio_dio_basico;
  * @author Windows 10 PRO
  */
 public class ContaPoupanca {
+    private int agencia;
+    private int numero;
+    private double salario;
     
+    public void sacar(){
+        
+    }
+    public void depositar(){
+        
+    }
+    public void transferir(){
+        
+    }
 }
